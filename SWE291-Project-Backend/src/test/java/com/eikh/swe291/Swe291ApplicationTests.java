ba01aef9e239e18567f8827b2020b3f0b7fd5e1d
duckmhe170996 <duckmhe170996@fpt.edu.vn>
2023-05-11 16:24:08 +0700

    initialize spring boot project

---
+++ SWE291-Project-Backend/src/test/java/com/eikh/swe291/Swe291ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eikh.swe291;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe291ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
