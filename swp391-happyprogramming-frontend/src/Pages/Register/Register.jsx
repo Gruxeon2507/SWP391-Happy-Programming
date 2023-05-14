95ba34754100af8740a24f15868de2b5cb16e444
giangpt <giangpthe170907@fpt.edu.vn>
2023-05-15 02:47:32 +0700

    add verify and register reactjs

--- swp391-happyprogramming-frontend/src/Pages/Register/Register.jsx
+++ swp391-happyprogramming-frontend/src/Pages/Register/Register.jsx
@@ -1,48 +1,116 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import NavBar from "../../Components/Navbar/NavBar";
 import "../../Components/Navbar/NavBar.css";
 import "./Register.css";
+import axios from "axios";
 import { Button } from "bootstrap";
 
 function Register(props) {
+
+  const [user,setUser] = useState({
+    username : "",
+    password : "",
+    displayName : "",
+    mail : "",
+  })
+  const [rePassword,setRePassword] = useState("");
+  const [checkRePassword,setCheckRePassword] = useState(true);
+  const [MessageRePassword,setMessageRePassword] = useState("");
+  const [messageVerify,setMessageVerify] = useState("");
+
+
+  const onChangeUsername = (event) => {
+    const inputUsername = event.target.value;
+    setUser({
+      ...user,
+      username : inputUsername,
+    })
+  }
+
+  const onChangePassword = (event) => {
+    const inputPassword = event.target.value;
+    setUser({
+      ...user,
+      password : inputPassword,
+    })
+  }
+
+  const onChangeRePassword = (event) => {
+    const inputRePassword = event.target.value;
+    if(inputRePassword != user.password){
+      setCheckRePassword(true);
+      setMessageRePassword("Re Password not match")
+    }else{
+      setCheckRePassword(false);
+    }
+  }
+  
+
+  const onChangeEmail = (event) => {
+    const inputEmail = event.target.value;
+    setUser({
+      ...user,
+      mail : inputEmail,
+    })
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(checkRePassword == true){
+      alert(MessageRePassword);
+      return;
+    }
+
+     axios.post("http://localhost:1111/api/auth/register",user)
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+    setMessageVerify("Please verify mail your account");
+
+  }
+
   return (
     <div>
       <NavBar></NavBar>
       <div className="regis-frag">
         <div className="regis-form">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-header">
               <h1>Registration form</h1>
             </div>
             <div className="user-input">
-              <input type="text" id="userName" required></input>
+              <input type="text" id="userName" required onChange={onChangeUsername}></input>
               <span>UserName</span>
-            </div>
+            </div>  
             <div className="user-input">
-              <input type="text" id="userPassword" required></input>
+              <input type="password" id="userPassword" required onChange={onChangePassword}></input>
               <span>Password</span>
             </div>
             <div className="user-input">
-              <input type="text" id="re-userPassword" required></input>
+              <input type="password" id="re-userPassword" required onChange={onChangeRePassword}></input>
               <span>Re Enter Password</span>
             </div>
+            {checkRePassword ? (
+              <>
+                <div style={{ height: "0px" }}></div>
+                <div >{MessageRePassword}</div>
+              </>
+            ) : null}
             <div className="user-input">
-              <input type="text" id="email-I" required></input>
+              <input type="email" id="email-I" required onChange={onChangeEmail}></input>
               <span>Email</span>
             </div>
-            <button type="submit">Register</button>
+            <button type="submit" >Register</button>
           </form>
+          {messageVerify != "" ? (<><div style={{height : "0px"}}></div>
+          <div>{messageVerify}</div></>) : null}
         </div>
-        <div className="confirm-email">
-          <div className="form-header">
-            <h1>OTP Verification</h1>
-          </div>
-          <div className="user-input">
-            <input type="text" id="cf-code-email" required></input>
-            <span>Enter Verification Code</span>
-          </div>
-          <button type="submit">confirm</button>
-        </div>
+        
       </div>
     </div>
   );
