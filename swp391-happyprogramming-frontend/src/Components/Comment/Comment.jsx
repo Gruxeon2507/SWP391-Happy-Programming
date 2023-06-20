bf207feb19cd2050dc2bb09c2f5f67b5150cdca9
antthe176160 <antthe176160@fpt.edu.vn>
2023-06-21 06:48:40 +0700

    comment, post layout

--- swp391-happyprogramming-frontend/src/Components/Comment/Comment.jsx
+++ swp391-happyprogramming-frontend/src/Components/Comment/Comment.jsx
@@ -15,7 +15,7 @@ const Comment = ({ comment, layer }) => {
   const inputRef = useRef(null);
   const [replies, setReplies] = useState([]);
 
-  useEffect(() => {}, [replies]);
+  useEffect(() => { }, [replies]);
 
   useEffect(() => {
     UserServices.getLoginUsername()
@@ -69,114 +69,121 @@ const Comment = ({ comment, layer }) => {
 
   return (
     <>
-      <div style={{ marginBottom: "-10px" }}>Input: {input}</div>
-      <h1 style={{ marginBottom: "0px" }}>
-        {`${
-          comment.user && comment.user.displayName
-            ? comment.user.displayName
-            : "Username failed to load"
-        } (ID: ${comment.commentId})`}
-      </h1>
-      {/* <div className="pcw-content" dangerouslySetInnerHTML={{ __html: post.postContent }} */}
+      <img className="cmt-avt"
+        src={`http://localhost:1111/api/users/avatar/${loginUsername}`}
+        alt="avatar"
+      ></img>
+      <div className="cmt-item-wrap">
+        <div className="cmt-content">
+          <div className="cmt-author-info">
 
-      <h1
-        contentEditable={editMode}
-        suppressContentEditableWarning={editMode}
-        style={{ marginTop: "0" }}
-        ref={inputRef}
-        dangerouslySetInnerHTML={{ __html: comment.commentContent }}
-      >
-        {/* {comment.commentContent} */}
-      </h1>
-      <div style={{ display: "flex" }}>
-        {editMode ? (
-          <>
-            <ActionButton
-              className="reply comment"
-              type="SAVE"
-              handleClick={() => onEditComment()}
-            ></ActionButton>
-            <ActionButton
-              className="reply comment"
-              type="CANCEL"
-              handleClick={() => setEditMode(false)}
-            ></ActionButton>
-          </>
-        ) : (
-          <>
-            {layer < 3 && (
-              <ActionButton
-                className="reply comment"
-                type={
-                  <>
-                    {expand ? (
-                      <AiOutlineCaretUp
-                        width="10px"
-                        height="10px"
-                      ></AiOutlineCaretUp>
-                    ) : (
-                      <AiOutlineCaretDown
-                        width="10px"
-                        height="10px"
-                      ></AiOutlineCaretDown>
-                    )}{" "}
-                    REPLY
-                  </>
-                }
-                handleClick={() => handleNewComment()}
-              ></ActionButton>
-            )}
-            {comment.user.username == loginUsername ? (
-              <>
+            <span style={{ marginBottom: "0px" }}>
+              {`${comment.user && comment.user.displayName
+                ? comment.user.displayName
+                : "Username failed to load"
+                } (ID: ${comment.commentId})`}
+            </span>
+          </div>
+
+          <span className="cmt-text"
+            contentEditable={editMode}
+            suppressContentEditableWarning={editMode}
+            style={{ marginTop: "0" }}
+            ref={inputRef}
+            dangerouslySetInnerHTML={{ __html: comment.commentContent }}
+          >
+            {/* {comment.commentContent} */}
+          </span>
+          <div style={{ display: "flex" }}>
+            {editMode ? (
+              <div className="rep-btn">
                 <ActionButton
-                  className="comment"
-                  type="EDIT"
-                  handleClick={() => setEditMode(true)}
+                  className="reply comment"
+                  type="SAVE"
+                  handleClick={() => onEditComment()}
                 ></ActionButton>
                 <ActionButton
                   className="reply comment"
-                  type="DELETE"
-                  handleClick={() => deleteComment()}
+                  type="CANCEL"
+                  handleClick={() => setEditMode(false)}
                 ></ActionButton>
-              </>
+              </div>
             ) : (
-              <></>
+              <div className="layer-1-bttn">
+                {layer < 3 && (
+                  <ActionButton
+                    className="reply comment"
+                    type={
+                      <>
+                        {expand ? (
+                          <AiOutlineCaretUp
+                            width="10px"
+                            height="10px"
+                          ></AiOutlineCaretUp>
+                        ) : (
+                          <AiOutlineCaretDown
+                            width="10px"
+                            height="10px"
+                          ></AiOutlineCaretDown>
+                        )}{" "}
+                        REPLY
+                      </>
+                    }
+                    handleClick={() => handleNewComment()}
+                  ></ActionButton>
+                )}
+                {comment.user.username == loginUsername ? (
+                  <>
+                    <ActionButton
+                      className="comment"
+                      type="EDIT"
+                      handleClick={() => setEditMode(true)}
+                    ></ActionButton>
+                    <ActionButton
+                      className="reply comment"
+                      type="DELETE"
+                      handleClick={() => deleteComment()}
+                    ></ActionButton>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
             )}
-          </>
-        )}
-      </div>
-
-      {/* --------------------SHOW REPLIES-------------------- */}
-      <div style={{ display: expand ? "block" : "none", paddingLeft: "100px" }}>
-        {showInput && (
-          <div className="inputContainer" style={{ display: "flex" }}>
-            <input
-              type="text"
-              className="inputContainter_input"
-              autoFocus
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <ActionButton
-              className="reply"
-              type="REPLY"
-              handleClick={() => addReply(comment)}
-            ></ActionButton>
-            <ActionButton
-              className="reply"
-              type="CANCEL"
-              handleClick={() => setShowInput(false)}
-            ></ActionButton>
           </div>
-        )}
-        {replies.map((reply) => (
-          <>
-            <Comment
-              comment={reply}
-              key={reply.commentId}
-              layer={layer + 1}
-            ></Comment>
-          </>
-        ))}
+        </div>
+        {/* --------------------SHOW REPLIES-------------------- */}
+        <div className="cmt-rep-1" style={{ display: expand ? "block" : "none" }}>
+          {showInput && (
+            <div className="inputContainer" style={{ display: "flex" }}>
+              <input
+                type="text"
+                className="inputContainter_input"
+                autoFocus
+                onChange={(e) => setInput(e.target.value)}
+              />
+              <ActionButton
+                className="reply"
+                type="REPLY"
+                handleClick={() => addReply(comment)}
+              ></ActionButton>
+              <ActionButton
+                className="reply"
+                type="CANCEL"
+                handleClick={() => setShowInput(false)}
+              ></ActionButton>
+            </div>
+          )}
+          {replies.map((reply) => (
+            <div className="cmt-card">
+              <Comment
+                comment={reply}
+                key={reply.commentId}
+                layer={layer + 1}
+              ></Comment>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
--- swp391-happyprogramming-frontend/src/Pages/PostDetail/PostDetail.jsx
+++ swp391-happyprogramming-frontend/src/Pages/PostDetail/PostDetail.jsx
@@ -5,6 +5,10 @@ import CreatePost from "../../Components/CreatePost/CreatePost";
 import Comment from "../../Components/Comment/Comment";
 import CommentServices from "../../services/CommentServices";
 import UserServices from "../../services/UserServices";
+import "./PostDetail.css";
+import NavBar from "../../Components/Navbar/NavBar";
+
+import sentIcon from "../../Assets/Send_Icon_039jcs.png";
 
 function PostDetail() {
   const { postId } = useParams("postId");
@@ -57,37 +61,55 @@ function PostDetail() {
     });
   };
 
-  useEffect(() => { }, []);
+  // useEffect(() => { }, []);
 
   return (
     <>
-      <div className="post-card-wrap" key={post.postId}>
-        <div>{post.postedAt}</div>
-        <div dangerouslySetInnerHTML={{ __html: post.postContent }} />
-      </div>
+      <NavBar mode={1} />
+      <main className="comment-main">
+        <div className="post-card-wrap" key={post.postId}>
+          <div>{post.postedAt}</div>
+          <div dangerouslySetInnerHTML={{ __html: post.postContent }} />
+        </div>
 
-      <hr></hr>
-      <div id={"comment-section"} style={{ paddingLeft: "50px" }}>
-        <input
-          type="text"
-          style={{ width: "25vw" }}
-          // autoFocus
-          value={input}
-          placeholder="type..."
-          onChange={(e) => setInput(e.target.value)}
-        />
-        {/* <div style={{ backgroundColor: "red" }}>{input}</div> */}
-        <button onClick={addComment}>Comment</button>
-        {comments.map((comment) => (
-          <>
-            <Comment
-              comment={comment}
-              key={comment.commentId}
-              layer={1}
-            ></Comment>
-          </>
-        ))}
-      </div>
+        <hr></hr>
+        <div id={"comment-section"} >
+
+          <div className="cmt-input">
+            <img
+              src={`http://localhost:1111/api/users/avatar/${loginUsername}`}
+              alt="avatar"
+            ></img>
+            <div className="cmt-input-text">
+              <input
+                type="text"
+                value={input}
+                placeholder="Write a comment..."
+                onChange={(e) => setInput(e.target.value)}
+              />
+              <ion-icon onClick={addComment} name="send"></ion-icon>
+
+              {/* <img
+                src={sentIcon}
+                alt="sent"
+              ></img> */}
+              {/* <div style={{ backgroundColor: "red" }}>{input}</div> */}
+              {/* <button id="comment-Bttn" onClick={addComment}>Comment</button> */}
+            </div>
+          </div>
+          <div className="cmt-list">
+            {comments.map((comment) => (
+              <div className="cmt-card">
+                <Comment
+                  comment={comment}
+                  key={comment.commentId}
+                  layer={1}
+                ></Comment>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
