31f72b47abed722ff778dc16e3e45d5cd93e94a8
antthe176160 <antthe176160@fpt.edu.vn>
2023-07-23 14:23:29 +0700

    layout placeholder for system stats

--- swp391-happyprogramming-frontend/src/Pages/Admin/SystemStats.jsx
+++ swp391-happyprogramming-frontend/src/Pages/Admin/SystemStats.jsx
@@ -1,62 +1,104 @@
 import React, { useState, useEffect } from "react";
+import "./SystemStats.css"
+import NavBar from "../../Components/Navbar/NavBar";
 
 const SystemStats = () => {
   return (
     <>
-      <div className="plain-stats">
-        <p>Users: 13</p>
-        <p>Courses: 20</p>
-        <p>Requests this month: 300</p>
-        <p>New courses this month: 300</p>
-      </div>
-      <div className="visual-stats">
-        <div className="bar-charts">
-          <div className="user-role-all">
-            <h3>Mentee/mentor/admin ratio</h3>
-            <div>The chart goes here</div>
-          </div>
-          <div className="user-status-all">
-            <h3>Active/inactive ratio</h3>
-            <div>The chart goes here</div>
-          </div>
-          <div className="request-status-all">
-            <h3>Approved/pending/rejected ratio</h3>
-            <div>The chart goes here</div>
-          </div>
+      <NavBar mode={1}></NavBar>
+      <main className="--outer">
+        <h1>System Statistic</h1>
+        <div className="--main-wraper">
+          <aside className="plain-stats">
+            <div className="--plain">
+              <span>13</span>
+              <span>Users</span>
+              <ion-icon name="people-outline"></ion-icon>
+            </div>
+            <div className="--plain">
+              <span>20</span>
+              <span>Course</span>
+              <ion-icon name="pricetags-outline"></ion-icon>
+            </div>
+            <div className="--plain">
+              <span>300</span>
+              <span>Requests this month</span>
+              <ion-icon name="pricetags-outline"></ion-icon>
+            </div>
+            <div className="--plain">
+              <span>300</span>
+              <span>New courses this month</span>
+              <ion-icon name="add-circle-outline"></ion-icon>
+            </div>
+
+          </aside>
+
+          <section className="--visual">
+            <div className="visual-stats">
+
+
+              <div className="--grid--chart">
+                <h3>Mentee/mentor/admin ratio</h3>
+                <div>The chart goes here</div>
+              </div>
+
+              <div className="--grid--chart">
+                <h3>Active/inactive ratio</h3>
+                <div>The chart goes here</div>
+              </div>
+
+              <div className="--grid--chart">
+                <h3>Approved/pending/rejected ratio</h3>
+                <div>The chart goes here</div>
+              </div>
+
+
+
+              <div className="--grid--chart">
+                <h3>Approved/pending/rejected ratio</h3>
+                <div>The chart goes here</div>
+              </div>
+
+              <div className="--grid--chart">
+                <h3>Approved/pending/rejected ratio</h3>
+                <div>The chart goes here</div>
+              </div>
+
+              <div className="--grid--chart">
+                <h3>Approved/pending/rejected ratio</h3>
+                <div>The chart goes here</div>
+              </div>
+
+
+            </div>
+
+            <div className="top-stats">
+
+              <div className="popular-course">
+                <h3>Top 3 courses with most users</h3>
+                <div>Stats go here</div>
+              </div>
+
+              <div className="active-mentor">
+                <h3>Top 3 mentors who join most courses</h3>
+                <div>Stats go here</div>
+              </div>
+
+              <div className="active-mentee">
+                <h3>Top 3 mentees who join most courses</h3>
+                <div>Stats go here</div>
+              </div>
+
+              <div className="reported-user">
+                <h3>Top 3 users who receive most reports</h3>
+                <div>Stats go here</div>
+              </div>
+
+            </div>
+
+          </section>
         </div>
-        <div className="line-charts">
-          <div className="user-role-course">
-            <h3>Approved/pending/rejected ratio</h3>
-            <div>The chart goes here</div>
-          </div>
-          <div className="user-status-course">
-            <h3>Approved/pending/rejected ratio</h3>
-            <div>The chart goes here</div>
-          </div>
-          <div className="request-status-course">
-            <h3>Approved/pending/rejected ratio</h3>
-            <div>The chart goes here</div>
-          </div>
-        </div>
-      </div>
-      <div className="top-stats">
-        <div className="popular-course">
-          <h3>Top 3 courses with most users</h3>
-          <div>Stats go here</div>
-        </div>
-        <div className="active-mentor">
-          <h3>Top 3 mentors who join most courses</h3>
-          <div>Stats go here</div>
-        </div>
-        <div className="active-mentee">
-          <h3>Top 3 mentees who join most courses</h3>
-          <div>Stats go here</div>
-        </div>
-        <div className="reported-user">
-          <h3>Top 3 users who receive most reports</h3>
-          <div>Stats go here</div>
-        </div>
-      </div>
+      </main>
     </>
   );
 };
